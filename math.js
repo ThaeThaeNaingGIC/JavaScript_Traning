e5c850b9ab6d1b83aa622c675321db08fcaa6fd1
thaethae <thaethaenaing@gicjp.com>
2023-08-08 17:43:24 +0630

    lession24_math

---
+++ math.js
@@ -0,0 +1,14 @@
+console.log(Math.pow(2, 4));
+console.log(Math.min(1, 4));
+
+let num = 2.9;
+console.log(Math.round(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Math.sqrt(num));
+console.log(Math.abs(num));
+
+for (let i = 0; i < 10; i++) {
+    let num = Math.ceil(Math.random() * 6 + 1);
+    console.log(num);
+}
