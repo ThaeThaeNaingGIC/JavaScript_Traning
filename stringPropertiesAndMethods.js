646f16d7bb604140dcd61795bd158e73042bf20c
thaethae <thaethaenaing@gicjp.com>
2023-08-09 10:46:52 +0630

    lession34_stringProperties

---
+++ stringPropertiesAndMethods.js
@@ -0,0 +1,18 @@
+let str = "Hey I am"
+let str2 = "Thae Thae"
+let string = str.concat("...", str2);
+console.log(string);
+console.log(str2.charAt(1));
+console.log(str2.charAt(str2.length - 1));
+console.log(str2.indexOf('a'));
+console.log(str2.lastIndexOf('a'));
+console.log(str2.substr(0, 3));
+console.log(str2.substr(-2, 3));
+console.log(str2.slice(-4));
+console.log(str2.slice(0, 1));
+console.log(str2[3]);
+console.log(str2.replace("Thae", "Cuttie"));
+
+
+
+
