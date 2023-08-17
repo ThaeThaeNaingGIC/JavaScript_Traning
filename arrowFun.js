cb436b2ef4968e9a54a6a6f2f22a9388e9af431d
thaethae <thaethaenaing@gicjp.com>
2023-08-17 17:16:13 +0630

    lession58_arrowFun.js

---
+++ arrowFun.js
@@ -0,0 +1,11 @@
+let students = [
+    {
+        "name": "Mg Mg",
+        "age": 22
+    },
+    {
+        "name": "Mg Mg",
+        "age": 15
+    }
+]
+students.map((std) => console.log(std.age));
